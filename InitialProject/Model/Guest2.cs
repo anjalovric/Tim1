7be61e0da969885fc4bd6b2a23049a08b3808432
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2023-03-06 23:32:36 +0100

    feature: Initial Model for Guest2

--- InitialProject/Model/Guest2.cs
+++ InitialProject/Model/Guest2.cs
@@ -8,8 +8,33 @@ using System.Xml.Linq;
 using InitialProject.Serializer;
 namespace InitialProject.Model
 {
-    public class Guest2 
+    public class Guest2 :ISerializable
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+
+
+
+        public Guest2() { }
+        public Guest2(string name, string lastName)
+        {
+            Name = name;
+            LastName = lastName;
+        }
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Name = values[1];
+            LastName = values[2];
+            Username = values[3];
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Name, LastName, Username };
+            return csvValues;
+        }
     }
 }
