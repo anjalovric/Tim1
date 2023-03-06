7a9579fc11e61839fd6b61385ed4a8bc2f15ae8f
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2023-03-06 21:38:16 +0100

    feature: Initial model for guest1

--- InitialProject/Model/Guest1.cs
+++ InitialProject/Model/Guest1.cs
@@ -7,8 +7,34 @@ using InitialProject.Serializer;
 
 namespace InitialProject.Model
 {
-    public class Guest1 
-    {
-        
-    }
+        public class Guest1 : ISerializable
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string LastName { get; set; }
+            public string Username { get; set; }
+
+            
+
+            public Guest1() { }
+            public Guest1(string name, string lastName)
+            {
+                Name = name;
+                LastName = lastName;
+            }
+            public void FromCSV(string[] values)
+            {
+                Id = Convert.ToInt32(values[0]);
+                Name = values[1];
+                LastName = values[2];
+                Username = values[3];
+            }
+
+            public string[] ToCSV()
+            {
+                string[] csvValues = { Id.ToString(), Name, LastName, Username };
+                return csvValues;
+            }
+        }
+    
 }
