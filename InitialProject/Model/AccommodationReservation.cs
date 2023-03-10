e2ad006f37c99f23e8fdae2176c0d7257f94ba93
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2023-03-10 17:04:19 +0100

    Feat: Entering date and number of days for reservations; Constraints for min. number of days for reservation

---
+++ InitialProject/Model/AccommodationReservation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using InitialProject.Serializer;
+
+namespace InitialProject.Model
+{
+    public class AccommodationReservation : ISerializable
+    {
+        public int Id { get; set; }
+        public int GuestId { get; set; }
+        public int AccommodationId { get; set; }
+
+        public AccommodationReservation() { }
+        public AccommodationReservation(int guestId, int accommodationId)
+        {
+            GuestId = guestId;
+            AccommodationId = accommodationId;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            GuestId = Convert.ToInt32(values[1]);
+            AccommodationId = Convert.ToInt32(values[2]);
+
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), GuestId.ToString(), AccommodationId.ToString()};
+            return csvValues;
+        }
+    }
+}
---
+++ InitialProject/View/AccommodationReservationForm.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using InitialProject.Model;
+
+namespace InitialProject.View
+{
+    /// <summary>
+    /// Interaction logic for AccommodationReservationForm.xaml
+    /// </summary>
+    public partial class AccommodationReservationForm : Window
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        TimeSpan difference { get; set; }
+
+       
+        private Accommodation currentAccommodation { get; set; }
+        
+        
+
+        public AccommodationReservationForm(Accommodation currentAccommodation)
+        {
+            InitializeComponent();
+            this.DataContext = this;
+            this.currentAccommodation = currentAccommodation;
+        }
+
+        private void DecrementDaysNumber(object sender, RoutedEventArgs e)
+        {
+            int changedDaysNumber;
+            if (Convert.ToInt32(numberOfDays.Text) > 1)
+            {
+                changedDaysNumber = Convert.ToInt32(numberOfDays.Text) - 1;
+                numberOfDays.Text = changedDaysNumber.ToString();
+            }
+        }
+
+        private void IncrementDaysNumber(object sender, RoutedEventArgs e)
+        {
+            int changedDaysNumber;
+            changedDaysNumber = Convert.ToInt32(numberOfDays.Text) + 1;
+            numberOfDays.Text = changedDaysNumber.ToString();
+        }
+
+        private void Confirm_Click(object sender, RoutedEventArgs e)
+        {
+            TimeSpan difference;
+            difference = EndDate.Subtract(StartDate);
+            int daysNumberFromCalendar = Convert.ToInt32(difference.TotalDays);
+
+            if(daysNumberFromCalendar < currentAccommodation.MinDaysForReservation || Convert.ToInt32(numberOfDays.Text) < currentAccommodation.MinDaysForReservation)
+            {
+                MessageBox.Show("The minimum number of days for booking this accommodation is " + currentAccommodation.MinDaysForReservation.ToString());
+            }
+            else
+            {
+                
+            }
+
+
+        }
+
+        private void CalendarStartDate_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            UIElement originalElement = e.OriginalSource as UIElement;
+            if (originalElement != null)
+            {
+                originalElement.ReleaseMouseCapture();
+            }
+        }
+        private void CalendarEndDate_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            UIElement originalElement = e.OriginalSource as UIElement;
+            if (originalElement != null)
+            {
+                originalElement.ReleaseMouseCapture();
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+       
+    }
+}
