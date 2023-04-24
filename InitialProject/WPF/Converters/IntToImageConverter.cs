b4715e452db9349b577466ebc86e4753a68a07b7
Anja Ducic <ducic.ra5.2020@uns.ac.rs>
2023-04-24 02:01:00 +0200

    Feat: renovation suggestion input conditions corrected; view fot reviews and average ratings corrected

---
+++ InitialProject/WPF/Converters/IntToImageConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace InitialProject.WPF.Converters
+{
+    public class IntToImageConverter : IValueConverter
+    {
+        object IValueConverter.Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var list = new List<string>();
+            if (value is int j)
+            {
+                for(int i=0; i<j; i++)
+                {
+                    list.Add("/Resources/Images/goldStar1.png");
+                }
+            }
+            return list;
+        }
+
+        object IValueConverter.ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- InitialProject/WPF/ViewModels/Guest1ViewModels/Guest1ReviewsViewModel.cs
+++ InitialProject/WPF/ViewModels/Guest1ViewModels/Guest1ReviewsViewModel.cs
@@ -19,6 +19,7 @@ namespace InitialProject.WPF.ViewModels.Guest1ViewModels
         public double AverageCleanliness { get; set; }
         public int ReviewsNumber { get; set; }
         public double AverageFollowingRules { get; set; }
+        public int AverageRating { get; set; }
 
         private ObservableCollection<GuestReview> guest1Reviews;
         public ObservableCollection<GuestReview> Guest1Reviews
@@ -45,6 +46,7 @@ namespace InitialProject.WPF.ViewModels.Guest1ViewModels
             AverageCleanliness = guestReviewService.GetAverageCleanlinessReview(guest1);
             AverageFollowingRules = guestReviewService.GetAverageFollowingRulesReview(guest1);
             ReviewsNumber = guestReviewService.GetReviewsNumberByGuest(guest1);
+            AverageRating = guestReviewService.GetAverageRating(guest1);
             
         }
         
