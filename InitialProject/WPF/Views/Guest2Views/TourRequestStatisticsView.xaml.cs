90b20526b44d49e575a5af98d752deb5c5b97086
Ivana Kovacevic <kovacevic.ra15.2020@uns.ac.rs>
2023-04-25 12:35:48 +0200

    Feat: Percent of accepted requests and invalid requests

--- InitialProject/WPF/Views/Guest2Views/TourRequestStatisticsView.xaml.cs
+++ InitialProject/WPF/Views/Guest2Views/TourRequestStatisticsView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using InitialProject.Service;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using InitialProject.Domain.Model;
 
 namespace InitialProject.WPF.Views.Guest2Views
 {
@@ -20,10 +23,44 @@ namespace InitialProject.WPF.Views.Guest2Views
     /// </summary>
     public partial class TourRequestStatisticsView : Window
     {
+        public double acceptedRequest { get; set; }
+        public double invalidRequest { get; set; }
+        private OrdinaryTourRequestsService ordinaryTourRequestsService;
+        private List<OrdinaryTourRequests> OrdinaryTourRequests;
         public TourRequestStatisticsView(Model.Guest2 guest2)
         {
             InitializeComponent();
+            DataContext = this;
+            acceptedRequest = 0;
+            invalidRequest = 0;
+            ordinaryTourRequestsService = new OrdinaryTourRequestsService();
+            OrdinaryTourRequests = new List<OrdinaryTourRequests>(ordinaryTourRequestsService.GetAll());
+            ProcentOfAcceptedRequest();
+            ProcentOfInvalidRequest();
         }
-
+        private void ProcentOfAcceptedRequest()
+        {
+            foreach(OrdinaryTourRequests request in OrdinaryTourRequests)
+            {
+                if (request.Status == "Valid")
+                {
+                    acceptedRequest++;
+                }
+            }
+            acceptedRequest /= OrdinaryTourRequests.Count();
+            acceptedRequest *= 100;
+        }
+        private void ProcentOfInvalidRequest()
+        {
+            foreach (OrdinaryTourRequests request in OrdinaryTourRequests)
+            {
+                if (request.Status == "Invalid")
+                {
+                    invalidRequest++;
+                }
+            }
+            invalidRequest /= OrdinaryTourRequests.Count();
+            invalidRequest *= 100;
+        }
     }
 }
