d41d5032b366d54786e7785d9d95c9613c362be4
Milica Vujić <milicavujic2001@gmail.com>
2023-05-05 23:08:50 +0200

    fix:Resolving conflicts in CreateOrdinaryRequestViewModel

--- InitialProject/WPF/ViewModels/Guest2ViewModels/CreateOrdinaryTourRequestViewModel.cs
+++ InitialProject/WPF/ViewModels/Guest2ViewModels/CreateOrdinaryTourRequestViewModel.cs
@@ -238,7 +238,7 @@ namespace InitialProject.WPF.ViewModels.Guest2ViewModels
                 MessageBox.Show("Niste dobro popunili polja!");
                 return;
             }
-            OrdinaryTourRequests request = new OrdinaryTourRequests(Name,Guest2.Id, Convert.ToInt32(Capacity.Text), newLocation, Description, SelectedLanguage, Convert.ToDateTime(Start), Convert.ToDateTime(End), false, "On waiting",Start.ToString().Split(" ")[0],End.ToString().Split(" ")[0],-1,createDate,false,-1);
+            OrdinaryTourRequests request = new OrdinaryTourRequests(Name,Guest2.Id, Convert.ToInt32(MaxGuests), newLocation, Description, SelectedLanguage, Convert.ToDateTime(Start), Convert.ToDateTime(End), false, "On waiting",Start.ToString().Split(" ")[0],End.ToString().Split(" ")[0],-1,createDate,false,-1);
             OrdinaryTourRequests savedRequest=requestService.Save(request);
             RequestNotification requestNotification = new RequestNotification(savedRequest.Id);
             RequestNotificationService requestNotificationService = new RequestNotificationService();
