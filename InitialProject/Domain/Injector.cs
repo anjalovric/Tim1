36555c7a5a4ee85c47b08b215da65c8323b04485
Milica Vujić <milicavujic2001@gmail.com>
2023-05-24 18:22:29 +0200

    feature:MyProfileView

--- InitialProject/Domain/Injector.cs
+++ InitialProject/Domain/Injector.cs
@@ -44,7 +44,8 @@ namespace InitialProject.Domain
         { typeof(IAccommodationRenovationRepository), new AccommodationRenovationRepository() },
         { typeof(INewTourNotificationRepository), new NewTourNotificationRepository() },
         { typeof(IReviewNotificationRepository), new ReviewNotificationRepository() },
-        { typeof(IRequestNotificationRepository), new RequestNotificationRepository() }
+        { typeof(IRequestNotificationRepository), new RequestNotificationRepository() },
+            {typeof(ISuperGuideRepository), new SuperGuideRepository() }
         };
 
         public static T CreateInstance<T>()
---
+++ InitialProject/Domain/RepositoryInterfaces/ISuperGuideRepository.cs
@@ -0,0 +1,17 @@
+﻿using InitialProject.Domain.Model;
+using InitialProject.Serializer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InitialProject.Domain.RepositoryInterfaces
+{
+    interface ISuperGuideRepository:IGenericRepository<SuperGuide>
+    {
+        public SuperGuide Save(SuperGuide superGuide);
+
+        public void Delete(SuperGuide superGuide);
+    }
+}
